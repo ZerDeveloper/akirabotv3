355a3a02e80626fbe9e7c7082fb0ee9626941f49
ZerDeveloper <77194076+ZerDeveloper@users.noreply.github.com>
2021-04-29 07:31:11 +0700

    Update sewabot.js

--- akiraganz/sewabot.js
+++ akiraganz/sewabot.js
@@ -1,15 +1,7 @@
 const sewabot = () => { 
 	return `
 *OPEN JASA SEWA BOT*
-
-Sewa Sebulan : 10k
-Sewa Permanen : 25k
-
-*Payment :*
-_Gopay, Dana,Qris dan Tsel_
-
-Minat?PC
-Wa.me/6282158549899
+Gada Capek Gua
 `
 }
 exports.sewabot = sewabot
